a34a9577ffc43ac2dc636073a18bd94a0f94ad4f
joegr <joe.grosso@cogeco.ca>
2019-11-12 22:46:40 -0500

    Create initial commit

---
+++ P1.py
@@ -0,0 +1,10 @@
+def P1(array, n):
+    numsSeen = {}
+    for elem in array:
+        if elem in numsSeen:
+            return True
+        ~numsSeen[n - elem] = elem
+    return False
+
+
+print(P1([10,10], 20))
